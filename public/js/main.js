fb7573f169a576bbc4ee714c220f85e535137ca0
wahyuhjr <wahyuhidjr11@gmail.com>
2021-10-28 19:29:38 +0700

    effect

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
-
-
-document.addEventListener("click", function(e){
-    if (e.target.classList.contains("card-img-top")){
+document.addEventListener("click", function(e) {
+    if (e.target.classList.contains("card-img-top")) {
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show();
     }
 })
+
+AOS.init({
+    // delay: 1500,
+    duration: 2000,
+    once: true,
+});
